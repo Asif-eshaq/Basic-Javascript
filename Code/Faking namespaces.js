89f168f912bd8aeae9ab478c8dd05860500b4da1
SM Asif Bin Eshaq <asif.eshaq@northsouth.edu>
2019-09-19 14:13:30 +0600

    Add files via upload

---
+++ Code/Faking namespaces.js
@@ -0,0 +1,25 @@
+var greet = 'Hello';
+var greet = 'Hola';
+
+console.log(greet);
+
+var english = {
+    greetings : {
+        new : 'Hello'
+    }
+};
+
+var spanish = {};
+
+spanish.greet = 'Hola';
+
+console.log(spanish);
+console.log(english);
+
+
+
+
+
+
+
+
