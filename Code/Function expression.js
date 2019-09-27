22aee907ffdf322436c7a97272c590862c191d93
fuzzy95 <asif.eshaq@northsouth.edu>
2019-09-27 21:02:56 +0600

    First commit

---
+++ Code/Function expression.js
@@ -0,0 +1,35 @@
+greet();
+
+function greet() {
+    console.log('Hello');
+}
+
+//Below an object is being created
+var anonymousGreet = function() {
+    console.log('Hello');
+}
+
+anonymousGreet();
+
+
+function log(a) {
+    console.log(a);
+}
+log('Hello');
+
+
+function log(b) {
+    console.log(b);
+}
+log({
+    greeting : 'Hello'
+});
+
+
+function log(c) {
+    console.log(c);
+}
+log(function() {
+    console.log('Hello');
+});
+
