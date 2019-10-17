02a376a692b23a69c9accb1397c0530fad3c4531
fuzzy95 <asif.eshaq@northsouth.edu>
2019-10-18 00:15:22 +0600

    Minor fix

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,8 +1,8 @@
 const geoCode = require('./Utils/geocode.js');
 const foreCast = require('./Utils/forcast.js');
 
-geoCode('New york', (error, data) => {
-    console.log(('Error', error));
+geoCode('Dhaka', (error, data) => {
+    console.log('Error', error);
     console.log('Data', data);
     foreCast(data.latitude, data.longitude, (error, data) => {
         console.log("Error", error);
