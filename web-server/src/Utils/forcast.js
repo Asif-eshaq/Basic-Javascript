c63c794936fdee2b711219edf9040acd92fd4d0c
fuzzy95 <asif.eshaq@northsouth.edu>
2019-10-20 05:38:17 +0600

    updated

---
+++ web-server/src/Utils/forcast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/61c7f70822e612a390695ff5e6d891cf/' + latitude +','+ longitude ;
+    request ({url, json : true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect weather services.', undefined);
+        } else if (body.error) {
+            callback('Unable to find.', undefined);
+        } else {
+            callback(undefined, body.daily.data[1].summary + ' It is currently ' + body.daily.data[1].temperatureHigh + ' Degrees out. There is a ' + body.daily.data[1].precipProbability + '% chance of rain.');
+        }
+    });
+}
+
+module.exports = forecast;
