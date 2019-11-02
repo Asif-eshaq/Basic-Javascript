3b74c46ec4bb3da6a430c63981048d6a22064357
fuzzy95 <asif.eshaq@northsouth.edu>
2019-11-02 21:20:38 +0600

    express middleware started

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,8 +6,24 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// register a middleware; runs between a request coming the server and rout handler running
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('Get request are disabled!');
+//     } else {
+//         next();
+//     }
+// });
 
+app.use((req, res, next) => {
+    if (req.method === 'GET' || req.method === 'POST' || req.method === 'PATCH' || req.method === 'DEL') {
+        res.status(503).send('Maintaining the sight');
+    } else {
+        next();
+    }
+})
+
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
@@ -15,18 +31,27 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
 
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
+
+// const myFunction = async () => {
+//     const password = '12345red';
+//     const hashPassword = await bcrypt.hash(password, 8);
+
+//     console.log(password);
+//     console.log(hashPassword);
+    
+//     const isMatch = await bcrypt.compare('12345red', hashPassword);
+//     console.log(isMatch);  
+// };
+
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const password = '12345red';
-    const hashPassword = await bcrypt.hash(password, 8);
+    const token = jwt.sign({_id : 'abc123'}, 'this is mynew course!', {expiresIn : '7 days'});
+    console.log(token);
 
-    console.log(password);
-    console.log(hashPassword);
-    
-    const isMatch = await bcrypt.compare('12345red', hashPassword);
-    console.log(isMatch);
-    
+    const data = jwt.verify(token, 'this is mynew course!');
+    console.log(data);
 };
 
 myFunction();
