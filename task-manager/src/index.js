99191b846d33fa4f94ffa2baeba9cbc00771f3fa
fuzzy95 <asif.eshaq@northsouth.edu>
2019-11-14 15:01:46 +0600

    file upload done

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -38,10 +38,14 @@ const port = process.env.PORT || 3000;
 //         // cb(undefined, false);
 //     }
 // });
+// // const errorMiddleware = (req, res, next) => {
+// //     throw new Error('From my middleware');
+// // };
 // app.post('/upload', upload.single('upload'), (req, res) => {
 //     res.send();
+// }, (error, req, res, next) => {
+//     res.status(400).send({error : error.message});
 // });
-
 
 
 app.use(express.json());
